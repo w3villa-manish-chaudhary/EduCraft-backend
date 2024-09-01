27aebbe5040c4f1c74c3238162a1341bf5c78c01
w3villa-manish-chaudhary <manish.chaudhary@w3villa.com>
2024-09-01 18:57:48 +0530

    Reordered and reformatted code

--- src/index.js
+++ src/index.js
@@ -2,30 +2,38 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const routes = require("./routes/index")
-const {initializeDatabase} = require('./database/dbconfig');
+const routes = require("./routes/index");
+const { initializeDatabase } = require('./database/dbconfig');
 const session = require('express-session');
 const passport = require('passport');
 require('./config/passport');
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 
-app.use(session({ secret: 'zxcvbnmasdfghjkl', resave: false, saveUninitialized: true }));
+app.use(session({
+    secret: 'zxcvbnmasdfghjkl',
+    resave: false,
+    saveUninitialized: true,
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
+// CORS configuration
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    credentials: true 
+}));
 
-const PORT = process.env.PORT || 8000;
-
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 initializeDatabase();
 
-app.use("/", routes)
+app.use("/", routes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
